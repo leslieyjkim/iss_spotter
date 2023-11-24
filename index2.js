9a89822b561a837bd84798842c22c07fec6fa9a4
yoonjookim <yjblossom@gmail.com>
2023-11-24 00:18:15 -0500

    promised version

---
+++ index2.js
@@ -0,0 +1,27 @@
+// const {
+//   fetchMyIP,
+//   fetchCoordsByIP,
+//   fetchISSFlyOverTimes,
+// } = require("./iss_promised");
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then((body) => console.log(body));
+
+const { nextISSTimesForMyLocation } = require("./iss_promised");
+
+const printPassTimes = function (passTimes) {
+  console.log(passTimes);
+};
+// see index.js for printPassTimes
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
